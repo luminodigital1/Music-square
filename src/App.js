acb243b0a21a73711147d608a9dd9dde2d59e081
luminodigital <lumino.digi@gmail.com>
2023-11-20 11:55:32 -0800

    code

--- src/App.js
+++ src/App.js
@@ -22,30 +22,30 @@ import Settings from './Pages/Settings/Settings';
 
 function App() {
   return(
-    <div></div>
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<LandingPage/>} />
-    //     <Route path="/signup" element={<Screen1/>} />
-    //     <Route path="/verify" element={<Screen2/>} />
-    //     <Route path='/profilesetup' element={<ProfileSetup/>}/>
-    //     <Route path='/profilesetup2' element={<ProfileSetup2/>}/>
-    //     <Route path='/profilesetup3' element={<ProfileSetup3/>}/>
-    //     <Route path='/homepage1' element={<Homepage1/>}/>
-    //     <Route path='/homepage2' element={<Homepage2/>}/>
-    //     <Route path='/findcollab' element={<FindCollab/>}/>
-    //     <Route path='/collabdetails' element={<CollabDetails/>}/>
-    //     <Route path="/collabstatus" element={<CollabStatus/>} />
-    //     <Route path="/collabstatusds" element={<CollabStatusDs/>} />
-    //     <Route path="/collabstatuscs" element={<CollabStatusCs/>} />
-    //     <Route path="/profile" element={<Profile />} />
-    //     <Route path="/collabcompleted" element={<CollabCompleted/>} />
-    //     <Route path="/postcollab" element={<PostCollab/>} />
-    //     <Route path="/chats" element={<Chats/>} />
-    //     <Route path="/communities" element={<Communities/>} />
-    //     <Route path="/settings" element={<Settings/>} />
-    //   </Routes>
-    // </Router>
+    //<div></div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage/>} />
+        <Route path="/signup" element={<Screen1/>} />
+        <Route path="/verify" element={<Screen2/>} />
+        <Route path='/profilesetup' element={<ProfileSetup/>}/>
+        <Route path='/profilesetup2' element={<ProfileSetup2/>}/>
+        <Route path='/profilesetup3' element={<ProfileSetup3/>}/>
+        <Route path='/homepage1' element={<Homepage1/>}/>
+        <Route path='/homepage2' element={<Homepage2/>}/>
+        <Route path='/findcollab' element={<FindCollab/>}/>
+        <Route path='/collabdetails' element={<CollabDetails/>}/>
+        <Route path="/collabstatus" element={<CollabStatus/>} />
+        <Route path="/collabstatusds" element={<CollabStatusDs/>} />
+        <Route path="/collabstatuscs" element={<CollabStatusCs/>} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/collabcompleted" element={<CollabCompleted/>} />
+        <Route path="/postcollab" element={<PostCollab/>} />
+        <Route path="/chats" element={<Chats/>} />
+        <Route path="/communities" element={<Communities/>} />
+        <Route path="/settings" element={<Settings/>} />
+      </Routes>
+    </Router>
   );
 }
 
